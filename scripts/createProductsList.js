9a3c5a6a93bbb1c80d5d70934a3074b8392043ba
Bryan Vera <brayanandresveragar@gmail.com>
2023-09-20 19:32:07 -0500

    :bug: Bug resuleto

--- scripts/createProductsList.js
+++ scripts/createProductsList.js
@@ -1,7 +1,7 @@
 //? Variables
 export const productList = []; // Producto1 Producto2 Producto3 Producto4 Producto5 Producto6
 
-function ingresarProducto(nombre,calificacion,comentarios,disponibilidad,descripcion,colores,precio,imagen) {
+function ingresarProducto(nombre,calificacion,comentarios,disponibilidad,descripcion,precio,imagen) {
     const producto = new Map();
 
     producto.set('nombre', nombre);
@@ -9,7 +9,6 @@ function ingresarProducto(nombre,calificacion,comentarios,disponibilidad,descrip
     producto.set('comentarios', comentarios);
     producto.set('disponibilidad', disponibilidad);
     producto.set('descripcion', descripcion);
-    producto.set('colores', colores);
     producto.set('precio', precio);
     producto.set('imagen', imagen);
     
@@ -22,7 +21,6 @@ ingresarProducto(
     10, //NUmero de comentarios
     true, //¿Esta disponible ?
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.', //Descripcion
-    ['red', 'blue', 'green'], //Colores
     12700, // Precio
     'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' //Imagen
 );
